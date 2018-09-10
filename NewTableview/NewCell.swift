f206bef84f7a61f9a7411e719e2f571f088f5f80
Naveen Chowdary <naveenchowdary@naveens-macbook-air.local>
2018-09-10 19:13:55 +0530

    added tableview and Delegate methods

---
+++ NewTableview/NewCell.swift
@@ -0,0 +1,25 @@
+//
+//  NewCell.swift
+//  NewTableview
+//
+//  Created by Naveen Chowdary on 10/09/18.
+//  Copyright © 2018 Naveen Chowdary. All rights reserved.
+//
+
+import UIKit
+
+class NewCell: UITableViewCell {
+
+    @IBOutlet weak var newlabel: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
